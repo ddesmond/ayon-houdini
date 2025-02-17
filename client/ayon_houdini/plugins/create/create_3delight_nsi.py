fa50de886fccc50481fa31b58505dad24f88b225
Aleks <crni.aki@gmail.com>
2025-02-17 05:54:43 +0100

    add aov setup

--- client/ayon_houdini/plugins/create/create_3delight_nsi.py
+++ client/ayon_houdini/plugins/create/create_3delight_nsi.py
@@ -48,16 +48,109 @@ class Create3DelightNsi(plugin.HoudiniCreator):
             product_name=product_name,
         )
 
+
         parms = {
             # Render frame range
             "trange": 1,
             # 3Delight ROP settings
+            # create params and setup the node
+            # quality
+            "shading_samples": 512,
+            "pixel_samples": 128,
+            "volume_samples": 1,
+
+            # scene elements
+            "override_display_flags": 1,
+            "objects_to_render": "RND_*",
+
+            # output
             "default_image_filename": filepath,
-            "default_export_nsi_filename": nsi_filepath,
-            "output_nsi_files": 1,
             "save_rendered_images": 0,
             "display_rendered_images": 0,
+            "output_nsi_files": 1,
+            "default_export_nsi_filename": nsi_filepath,
+
+            # image_layers
+            "aov": 16,
+            "add_layer": 16,
+
+            "multi_light_selection": "selection",
+            "light_sets": 2,
+
+            # set aovs
+            "active_layer_1": 1,
+            "aov_name_1": "Ci",
+            "aov_clear_1,": 0,
+
+            "active_layer_2": 1,
+            "aov_name_2": "Diffuse",
+            "aov_clear_2,": 0,
+
+            "active_layer_3": 1,
+            "aov_name_3": "Reflection",
+            "aov_clear_3,": 0,
+
+            "active_layer_4": 1,
+            "aov_name_4": "Refraction",
+            "aov_clear_4,": 0,
+
+            "active_layer_5": 1,
+            "aov_name_5": "Incandescence",
+            "aov_clear_5,": 0,
+
+            "active_layer_6": 1,
+            "aov_name_6": "Z (depth)",
+            "aov_clear_6,": 0,
+
+            "active_layer_7": 1,
+            "aov_name_7": "World Space Position",
+            "aov_clear_7,": 0,
+
+            "active_layer_8": 1,
+            "aov_name_8": "World Space Normal",
+            "aov_clear_8,": 0,
+
+            "active_layer_9": 1,
+            "aov_name_9": "Surface Shader Cryptomatte",
+            "aov_clear_9,": 0,
+
+            "active_layer_10": 1,
+            "aov_name_10": "Ambient Occlusion",
+            "aov_clear_10,": 0,
+
+            "active_layer_11": 1,
+            "aov_name_11": "Albedo",
+            "aov_clear_11,": 0,
+
+            "active_layer_12": 1,
+            "aov_name_12": "UV",
+            "aov_clear_12,": 0,
+
+            "active_layer_13": 1,
+            "aov_name_13": "Geometry Cryptomatte",
+            "aov_clear_13,": 0,
+
+            "active_layer_14": 1,
+            "aov_name_14": "Ci (direct)",
+            "aov_clear_14,": 0,
+
+            "active_layer_15": 1,
+            "aov_name_15": "Ci (indirect)",
+            "aov_clear_15,": 0,
+
+            "active_layer_16": 1,
+            "aov_name_16": "Shadow Mask",
+            "aov_clear_16,": 0,
+
+            "use_light_set_1": 1,
+            "light_set_1": "/obj/ambient",
+            "use_rgba_only_set_1": 1,
+            "use_light_set_2": 1,
+            "light_set_2": "/obj/distantlight1",
+            "use_rgba_only_set_2": 1
+
         }
+
 
         instance_node.setParms(parms)
 
